b34b7473174b298d46613e4e13c4dd0bf99a616f
ymilagroscordoba-sys <ymilagroscordoba@gmail.com>
2026-06-02 17:12:31 -0300

    Delete src directory

--- src/components/SkillBadge.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default function SkillBadge({ nombre }) {
-  return <span className="badge bg-primary me-2">{nombre}</span>;
-}
-
-SkillBadge.propTypes = {
-  nombre: PropTypes.string.isRequired,
-};
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import App from './App.jsx';
-ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App/></HashRouter>);
