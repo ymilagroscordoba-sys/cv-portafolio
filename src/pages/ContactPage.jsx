30d5bf4e7e9ae00b0163443323529b25d0ab3e69
ymilagroscordoba-sys <ymilagroscordoba@gmail.com>
2026-06-02 16:21:06 -0300

    Delete src directory

--- src/pages/ContactPage.jsx
+++
@@ -1,2 +0,0 @@
-
-export default function ContactPage(){return <div className='container mt-5'><h2>Contacto</h2><p>ymilagroscordoba@gmail.com</p><p>GitHub: ymilagroscordoba-sys</p></div>}
--- src/pages/ProjectsPage.jsx
+++
@@ -1,3 +0,0 @@
-
-import proyectos from '../data/proyectos.json';
-export default function ProjectsPage(){return <div className='container mt-5'><h2>Proyectos</h2>{proyectos.map(p=><div key={p.id}><h4>{p.nombre}</h4><p>{p.descripcion}</p></div>)}</div>}
